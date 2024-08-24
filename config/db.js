d0f51e9cc324384ec565dcb66a0fd7cea6d59aa0
mohamed rego <mohamedmrslan@gmail.com>
2024-08-24 20:23:23 +0300

    update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const URL = process.env.MONGODB_URL;
-const connectDB = () =>  mongoose.connect(URL).then(() => {
+const connectDB = () =>  mongoose.connect(URL , {
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  connectTimeoutMS: 10000
+}).then(() => {
   console.log("connected");
   require("../utils/deleteInActiveUsers.js");
 }).catch((error)=> {
