550c2bb2e2d628b340b458aa89788851f0a5d3be
mohamed rego <mohamedmrslan@gmail.com>
2024-08-31 15:07:33 +0300

    update

--- Router/authRoute.js
+++ Router/authRoute.js
@@ -26,36 +26,36 @@ router.route("/logout")
                       .post(logoutController)
 
 
-// router.get("/google" , passport.authenticate("googleRegister" , {
-//   scope: ["profile" , "email"]
-// }));
-
-// router.get("/google/callback" , passport.authenticate("googleRegister" ,
-//   { session: false }
-// ) , 
-//   (req , res) => {
-//     if(req.authInfo && req.authInfo.message === "User already exists"){
-//       return res.status(400).json({message: "User already exists"})
-//     }
-//     const { token , user } = req.user;
-//     res.status(200).json({message: "register successfully" , user , token})
-//   });
-
-
-
-
-
-
-
-
-
-
-router.get("/google" , passport.authenticate({
+router.get("/google/register" , passport.authenticate("googleRegister" , {
   scope: ["profile" , "email"]
 }));
 
-router.get("/google/callback" , (req ,res ,next) => {
-  passport.authenticate({ session: false } , (err , user , info) => {
+router.get("/google/register/callback" , passport.authenticate("googleRegister" ,
+  { session: false }
+) , 
+  (req , res) => {
+    if(req.authInfo && req.authInfo.message === "User already exists"){
+      return res.status(400).json({message: "User already exists"})
+    }
+    const { token , user } = req.user;
+    res.status(200).json({message: "register successfully" , user , token})
+  });
+
+
+
+
+
+
+
+
+
+
+router.get("/google/login" , passport.authenticate("googleLogin" , {
+  scope: ["profile" , "email"]
+}));
+
+router.get("/google/login/callback" , (req ,res ,next) => {
+  passport.authenticate("googleLogin" ,{ session: false } , (err , user , info) => {
     if(err){
       return next(err);
     }
--- config/passport.js
+++ config/passport.js
@@ -6,49 +6,49 @@ const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 
 
-// passport.use("googleRegister" , new GoogleStratgy({
-//   clientID: process.env.CLIENT_ID,
-//   clientSecret: process.env.CLIENT_SECRET,
-//   callbackURL: process.env.CLIENT_URL_REGISTER
-// },
-//   async(accessToken , refreshToken , profile , done) => {
-//     try {
-//       let user = await User.findOne({email: profile.emails[0].value});
-//       if(user){
-//         return done(null , user , {message: "User already exists"})
-//       }
+passport.use("googleRegister" , new GoogleStratgy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.CLIENT_URL_REGISTER
+},
+  async(accessToken , refreshToken , profile , done) => {
+    try {
+      let user = await User.findOne({email: profile.emails[0].value});
+      if(user){
+        return done(null , user , {message: "User already exists"})
+      }
 
-//       const password = crypto.randomBytes(16).toString("hex");
+      const password = crypto.randomBytes(16).toString("hex");
 
-//       const firstName = profile.name.givenName || "";
-//       const lastName = profile.name.familyName || "";
+      const firstName = profile.name.givenName || "";
+      const lastName = profile.name.familyName || "";
 
-//       const salt = await bcryptjs.genSalt(10);
-//       const hashPassword = await bcryptjs.hash(password , salt);
-//       user = await User({
-//         firstName: firstName,
-//         lastName: lastName,
-//         email: profile.emails[0].value,
-//         image: profile.photos[0].value,
-//         password: hashPassword,
-//         registed: true,
-//         vereificationCode: null
-//       });
-//       await user.save();
+      const salt = await bcryptjs.genSalt(10);
+      const hashPassword = await bcryptjs.hash(password , salt);
+      user = await User({
+        firstName: firstName,
+        lastName: lastName,
+        email: profile.emails[0].value,
+        image: profile.photos[0].value,
+        password: hashPassword,
+        registed: true,
+        vereificationCode: null
+      });
+      await user.save();
 
-//       const token = await jwt.sign({
-//         id: user._id,
-//         isAdmin: user.isAdmin
-//       }, process.env.JWT_SECRET_KEY , { expiresIn: "1d"});
+      const token = await jwt.sign({
+        id: user._id,
+        isAdmin: user.isAdmin
+      }, process.env.JWT_SECRET_KEY , { expiresIn: "1d"});
       
-//       return done(null , {user , token});
-//     } catch (err) {
-//       done(err , false)
-//     }
-//   }
-// ));
+      return done(null , {user , token});
+    } catch (err) {
+      done(err , false)
+    }
+  }
+));
 
-passport.use(new GoogleStratgy({
+passport.use("googleLogin", new GoogleStratgy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: process.env.CLIENT_URL_LOGIN
