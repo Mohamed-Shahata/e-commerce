d7049ba3b29040d835fa2c69795c5324fe5fddd4
mohamed rego <mohamedmrslan@gmail.com>
2024-09-01 12:38:58 +0300

    update

--- Router/authRoute.js
+++ Router/authRoute.js
@@ -78,23 +78,12 @@ router.get("/google/register/callback" , passport.authenticate("googleRegister" 
 
       const accessToken = req.authInfo.accessToken
       const createRefreshToken = req.authInfo.refreshToken
-      console.log(user + "\n" + createRefreshToken + "\n" + accessToken)
-      console.log("\n" + JSON.stringify(req.authInfo, null, 2));
-
-
-      
 
       res.cookie("refreshToken", createRefreshToken ,{
         httpOnly: true,
         secure: true,
         sameSite: "strict"
       })
-
-      console.log(req.cookies);
-      console.log("RefreshToken: " + req.cookies.refreshToken);
-
-      res.redirect("/")
-
 
       res.status(200).json({ message: 'Login successfully', user, accessToken });
     })
