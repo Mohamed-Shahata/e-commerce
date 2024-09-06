463a4adb869d0cd643834a12442bed3674c3d9da
mohamed <mohamedmrslan@gmail.com>
2024-09-06 23:24:21 +0300

    update

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ const app = express();
 connectDB();
 
 //middlewares
-app.use(cors({
-    credentials: true, 
-  }));
+app.use(cors());
 app.use(compression());
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true }));
