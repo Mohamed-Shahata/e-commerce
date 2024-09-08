71cf9cca883547c56cf46c5a6a27df549c68e328
mohamed <mohamedmrslan@gmail.com>
2024-09-08 04:18:55 +0300

    update

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = async(req , res , next) => {
 
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.headers.Authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Access denied. No token provided"});
