7258fb04d2856e25676fb0d4e912281a3e31ff0a
mohamed <mohamedmrslan@gmail.com>
2024-09-28 14:05:06 +0300

    Resolved merge conflicts in passwordRoute.js

--- Router/passwordRoute.js
+++ Router/passwordRoute.js
@@ -9,8 +9,4 @@ router.route("/forgot-password").post(sendForgotPasswordLink);
 
 router.route("/rest-password/:id/:token").post(resetThePassword);
 
-<<<<<<< HEAD
-module.exports = router;
-=======
 export default router;
->>>>>>> c3be5061bc9dc70aa8a126f29177a0fe0b44a23c
