b83be8e8e6db21b5cb679c51abb86bdd8d02050c
mohamed <mohamedmrslan@gmail.com>
2024-10-18 14:21:33 +0300

    update

--- index.js
+++ index.js
@@ -27,33 +27,26 @@ app.use(
 );
 
 app.use(compression());
-app.use(json({ limit: "50mb" }));
-app.use(urlencoded({ extended: true }));
-// app.use(static(join(__dirname, "images")));
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname , "images")));
 app.use(cookieParser());
 
+
 //setting db/passport
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 //setting ejs
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("view engine" , "ejs");
+app.set("views" , "./views");
 
 //Routes
-app.use("/password-mobile" , passwordMobileRoute);
-app.use("/api/auth", authRouter);
-app.use("/api/users", usersRoute);
-app.use("/password", passwordRoute);
-app.use("/api/products", productsRoute);
-app.use("/api", payment);
+app.use("/api/auth" , authRoute);
+app.use("/api/users" , usersRoute);
+app.use("/password" , passwordRoute);
+app.use("/api/products" , productsRoute);
+app.use("/api" , payment);
 
-// Global Error
-app.use((err, req, res, next) => {
-  const { status, message, stack } = err;
-  res
-    .status(status || 500)
-    .json(message, ...(process.env.MODE === "development" && { stack }));
-});
 //listen server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("server is live"));
+app.listen(PORT , () => console.log("server is live"));
